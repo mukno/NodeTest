562214a29f7ca3f6e85e623186f69bf2cfd7594c
mukno <52941742+mukno@users.noreply.github.com>
2021-02-02 23:55:25 +0900

    Revert "정보 보호"
    This reverts commit 86052715b0f10f1c246b6d2d89802913e747a357.

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://mukno:yy951753!@@bolierplate.o8ggg.mongodb.net/mukno?retryWrites=true&w=majority'
-}
--- index.js
+++ index.js
@@ -1,57 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser');
-const {User} = require("./models/User");
-
-const config = require=('./config/key');
-
-//application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:true}));
-
-//application/json
-app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect(config.mongoURI,{
+mongoose.connect('mongodb+srv://mukno:yy951753!@@bolierplate.o8ggg.mongodb.net/mukno?retryWrites=true&w=majority',{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected..'))
 .catch(err=>console.log(err))
 
-
-
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello Wosfsfasfrld!')
+  res.send('Hello World!')
 })
-
-
-app.post('/register',(req,res)=>{
-  //회원가입할때 필요한 정보들은 client에서 가져오면
-  //그것들을 데이터 베이스에 넣어준다.
-
-  const user=new User(req.body)
-
-  user.save((err,userInfo)=>{
-    if(err) return res.json({success:false,err})
-    return res.status(200).json({
-      success:true
-
-    })
-
-  })
-
-
-})
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
